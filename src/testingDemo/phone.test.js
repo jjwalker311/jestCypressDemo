05318072407927c2fdb4b78b81d90b409ce10c7c
johnathan.walker@vodafone.com <jonathanwalker@Jonathans-MacBook-Pro.local>
2018-11-26 10:00:20 +0000

    Updating examples leveraging the runners

--- src/testingDemo/phone.test.js
+++ src/testingDemo/phone.test.js
@@ -2,42 +2,27 @@ import validator, {parseDates} from './phone'
 import { neapolitan, vanilla } from './runner'
 import { validPhoneNumbers, invalidPhoneNumbers } from './data'
 
-describe('Run suite of tests for valid phonenumbers', ()=>{
+describe('Testing the Vanilla test runner using Sum', () => {
+    const sum = (a,b) => a + b
 
-    it('should call one off method to validate mobile number', ()=>{
-        expect(validator('07595627704')).toBe(true)
-    })
+    //[
+    //  [ExpectedInput, ExpectedInput],...
+    //]
+    const args = [
+        [[1,2], 3],
+        [[2,7], 9],
+        [[5,5], 10],
+        [[112,2], 114],
+        [[1,0], 1]
+    ]
 
-    it('should call forEach method to validate mobile number', ()=>{
-        validPhoneNumbers.forEach(phoneNumber => {
-            expect(validator(phoneNumber)).toBe(true)
-        })
-    })
-
-    //Values in Array passed into "each" can be accessed from function args
-    it.each(validPhoneNumbers)(
-        'should validate %s as true',
-        number => {
-            expect(validator(number)).toBe(true);
-        },
-    );
-
-    let runnerArgs = validPhoneNumbers.reduce((args, number) => ({
-            ...args,
-            [`should validate ${number} as true`] : {
-                input : number,
-                output : true
-                //?skip : Boolean
-                //?only : Boolean
-            }
-    }), {})
-
-    neapolitan(validator, runnerArgs)
+    vanilla(sum, args)
 })
 
-describe('parseDates Test', ()=>{
+describe('Testing the Neapolitan test runner using parseDates', ()=>{
 
     const input = {date : new Date()}
+
     const dateArgs = {
         ['should return all the dates'] : {
           input,
@@ -70,20 +55,52 @@ describe('parseDates Test', ()=>{
 
 })
 
+describe('Run Neapolitan for valid phonenumbers test', ()=>{
 
-describe('Testing the vanilla test runner', () => {
-    const sum = (a,b) => a + b
+    //Basic methods of demo purposes
+    // it('should call one off method to validate mobile number', ()=>{
+    //     expect(validator('07595627704')).toBe(true)
+    // })
 
-    //[
-    //  [ExpectedInput, ExpectedInput],...
-    //]
-    const args = [
-        [[1,2], 3],
-        [[2,7], 9],
-        [[5,5], 10],
-        [[112,2], 114],
-        [[1,0], 1]
-    ]
+    // it('should call forEach method to validate mobile number', ()=>{
+    //     validPhoneNumbers.forEach(phoneNumber => {
+    //         expect(validator(phoneNumber)).toBe(true)
+    //     })
+    // })
 
-    vanilla(sum, args)
+    // //Values in Array passed into "each" can be accessed from function args
+    // it.each(validPhoneNumbers)(
+    //     'should validate %s as true',
+    //     number => {
+    //         expect(validator(number)).toBe(true);
+    //     },
+    // );
+
+    //Reduce Array of valid numbers
+    const validArgs = validPhoneNumbers.reduce((args, number) => ({
+            ...args,
+            [`should validate ${number} as true`] : {
+                input : number,
+                output : true
+                //?skip : Boolean
+                //?only : Boolean,
+                //?assertion : Function
+            }
+    }), {})
+
+    neapolitan(validator, validArgs)
+
+    //Reduce Array of invalid numbers
+    const invalidArgs = invalidPhoneNumbers.reduce((args, number) => ({
+            ...args,
+            [`should validate ${number} as false`] : {
+                input : number,
+                output : false
+                //?skip : Boolean
+                //?only : Boolean
+                //?assertion : Function
+            }
+    }), {})
+
+    neapolitan(validator, invalidArgs)
 })
